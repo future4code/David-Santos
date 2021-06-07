553b0405948689b3ef73a731f71cfc2b2c3e24d0
0rtizD <davidortizdosantos@gmail.com>
2021-06-07 16:48:32 -0300

    Formulario- aula 21

---
+++ semana6/aula21-formulario/src/components/Etapa1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Etapa1 extends React.Component {
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+ 
---
+++ semana6/aula21-formulario/src/components/Etapa2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Etapa2 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Etapa2
